8228641e70ffbc1cb7f5efa22fcaf16e5163c735
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2025-11-12 17:04:29 +0000

    feat: implement 3D product viewer section

---
+++ src/store/index.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+type Store = {
+  color: string;
+  setColor: (color: string) => void;
+  scale: number;
+  setScale: (scale: number) => void;
+  //   texture: string;
+  //   setTexture: (texture: string) => void;
+  reset: () => void;
+};
+
+const useMacbookStore = create<Store>((set) => ({
+  color: "#2e2c2e",
+  setColor: (color) => set({ color }),
+
+  scale: 0.08,
+  setScale: (scale) => set({ scale }),
+
+  //   texture: "/videos/feature-1.mp4",
+  //   setTexture: (texture) => set({ texture }),
+
+  reset: () =>
+    // set({ color: "#2e2c2e", scale: 0.08, texture: "/videos/feature-1.mp4" }),
+    set({ color: "#2e2c2e", scale: 0.08 }),
+}));
+
+export default useMacbookStore;
